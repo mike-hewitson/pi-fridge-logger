940d11b367c313d14d1654a5014654546a8f74c3
mikeyjcat <mike@hewitson.co.za>
2016-05-02 17:12:59 +0200

    Add other sensors

--- index.js
+++ index.js
@@ -19,22 +19,53 @@ var logger = new winston.Logger({
     ]
 });
 
+// var sensor = {
+//     initialize: function() {
+//         return sensorLib.initialize(22, 4);
+//     },
+//     read: function() {
+//         var readout = sensorLib.read();
+//         logger.info('Temperature: ' + readout.temperature.toFixed(2) + 'C, ' +
+//             'humidity: ' + readout.humidity.toFixed(2) + '%');
+//         setTimeout(function() {
+//             sensor.read();
+//         }, 2000);
+//     }
+// };
+
+// if (sensor.initialize()) {
+//     sensor.read();
+// } else {
+//     logger.warn('Failed to initialize sensor');
+// }
+
+var sensorLib = require("node-dht-sensor");
+
 var sensor = {
-    initialize: function() {
-        return sensorLib.initialize(22, 4);
-    },
+    sensors: [{
+        name: "Ambient",
+        type: 22,
+        pin: 4
+    }, {
+        name: "Curing",
+        type: 22,
+        pin: 25
+    }, {
+        name: "Fridge",
+        type: 22,
+        pin: 24
+    }],
     read: function() {
-        var readout = sensorLib.read();
-        logger.info('Temperature: ' + readout.temperature.toFixed(2) + 'C, ' +
-            'humidity: ' + readout.humidity.toFixed(2) + '%');
+        for (var a in this.sensors) {
+            var b = sensorLib.readSpec(this.sensors[a].type, this.sensors[a].pin);
+            logger.info(this.sensors[a].name + ": " +
+                b.temperature.toFixed(1) + "C, " +
+                b.humidity.toFixed(1) + "%");
+        }
         setTimeout(function() {
             sensor.read();
         }, 2000);
     }
 };
 
-if (sensor.initialize()) {
-    sensor.read();
-} else {
-    console.warn('Failed to initialize sensor');
-}
+sensor.read();
